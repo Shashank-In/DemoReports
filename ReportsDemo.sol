d00e60222c9d80782f7697212b3b3220a8012387
Shashank <Shashank-In@users.noreply.github.com>
2023-11-22 14:41:18 +0530

    Update ReportsDemo.sol

--- ReportsDemo.sol
+++ ReportsDemo.sol
@@ -1,35 +1,42 @@
-pragma solidity 0.6.0;
-
-contract C {
-    
-    event Transfer(address s, uint amount);
-    
-    function g(address s) returns(address) {
-        return s;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract DamnVulnerableContract is ERC20, Ownable {
+    mapping(address => uint) public balances;
+    address public someOracle;
+    address[] users;
+    constructor(uint256 initialSupply, address _someOracle) ERC20("SolidityScan", "SCN") public {
+        _mint(msg.sender, initialSupply);
+        someOracle = _someOracle;
     }
-    
-    function badPractice() {
-        // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  adc165
-        address x = 0xf64B584972FE6055a770477670208d737Fff282f;
-        // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  adc165
-        x = g(0x72ba7d8e73fe8eb666ea66babc8116a41bfb10e2);
-        // too short - not an address
-        x = 0x123;
-        // <yes> <report> SOLIDITY_ADDRESS_HARDCODED  f32db1
-        x = address(342);
-        x = address(0);        
-        x = g(address(0));
-        x = 0x0;
-        x = g(0x0);
+    function mint(address _account, uint256 _amount) external {
+        _mint(_account, _amount);
+    } 
+    function deposit(address _to) external payable {
+        balances[_to] += msg.value;
+        users.push(_to);
     }
-    
-    function goodPractice(address _token, uint balance) {
-
-        if ((address(0) == _token)||(0x0 == _token)) {
-            Transfer(address(0), balance);
+    function withdraw(uint _amount) public {
+    require(address(this).balance == 10 ether, "This will probably throw due to incorrect logic");
+        if(balances[msg.sender] >= _amount) {
+            (bool result,) = msg.sender.call{value:_amount}("");
+                if(result) {
+                    _amount;
+                }
+            balances[msg.sender] -= _amount;
         }
-        if ((address(0) != _token)||(0x0 != _token)) {
-            Transfer(0x0, balance);
-        }      
+    }
+    function fetchPrice() public returns (bytes memory) {
+        (bool success, bytes memory data) = someOracle.call{gas: 5000}(abi.encodeWithSignature("priceFeed(uint256)", 123));
+        return data;
+    }
+    function calcFunc(uint256 _a, uint256 _b) external pure returns (uint256){
+        return (_a + _b)*100000000;
+    }
+    function payRewards() external onlyOwner {
+        for (uint256 i = 0; i < users.length; i++) {
+            transfer(users[i], balances[users[i]]);
+        }
     }
 }
